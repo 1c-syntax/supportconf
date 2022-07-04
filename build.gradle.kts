66b626c2b446047b81b4c425ebcc051d1f22775e
theshadowco <maximovvalery@gmail.com>
2022-07-04 16:29:34 +0300

    Перенос функциональности из mdclasses

---
+++ build.gradle.kts
@@ -0,0 +1,156 @@
+import java.util.*
+
+plugins {
+    `java-library`
+    `maven-publish`
+    jacoco
+    id("org.cadixdev.licenser") version "0.6.1"
+    id("com.github.gradle-git-version-calculator") version "1.1.0"
+    id("io.freefair.lombok") version "6.4.3"
+    id("io.freefair.javadoc-links") version "6.4.2"
+    id("io.freefair.javadoc-utf-8") version "6.4.3"
+    id("org.sonarqube") version "3.3"
+}
+
+group = "io.github.1c-syntax"
+version = gitVersionCalculator.calculateVersion("v")
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    // логирование
+    implementation("org.slf4j", "slf4j-api", "1.7.30")
+
+    // прочее
+    implementation("commons-io", "commons-io", "2.8.0")
+    implementation("io.github.1c-syntax", "bsl-common-library", "904b9172")
+
+    // тестирование
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.7.0")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.7.0")
+    testImplementation("org.assertj", "assertj-core", "3.18.1")
+    // логирование
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-log4j12
+    testImplementation("org.slf4j", "slf4j-log4j12", "1.7.30")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed", "standard_error")
+    }
+
+    reports {
+        html.required.set(true)
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(File("$buildDir/reports/jacoco/test/jacoco.xml"))
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-Xlint:unchecked")
+    options.compilerArgs.add("-parameters")
+}
+
+sonarqube {
+    properties {
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.organization", "1c-syntax")
+        property("sonar.projectKey", "1c-syntax_supportconf")
+        property("sonar.projectName", "Support Configuration")
+        property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacoco.xml")
+    }
+}
+
+artifacts {
+    archives(tasks["jar"])
+    archives(tasks["sourcesJar"])
+    archives(tasks["javadocJar"])
+}
+
+license {
+    header(rootProject.file("license/HEADER.txt"))
+    newLine(false)
+    ext["year"] = "2019 - " + Calendar.getInstance().get(Calendar.YEAR)
+    ext["name"] = "Tymko Oleg <olegtymko@yandex.ru>, Maximov Valery <maximovvalery@gmail.com>"
+    ext["project"] = "Support Configuration"
+    exclude("**/*.properties")
+    exclude("**/*.orig")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                description.set("Support configuration read library for Language 1C (BSL)")
+                url.set("https://github.com/1c-syntax/supportconf")
+                licenses {
+                    license {
+                        name.set("GNU LGPL 3")
+                        url.set("https://www.gnu.org/licenses/lgpl-3.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("otymko")
+                        name.set("Oleg Tymko")
+                        email.set("olegtymko@yandex.ru")
+                        url.set("https://github.com/otymko")
+                        organization.set("1c-syntax")
+                        organizationUrl.set("https://github.com/1c-syntax")
+                    }
+                    developer {
+                        id.set("theshadowco")
+                        name.set("Valery Maximov")
+                        email.set("maximovvalery@gmail.com")
+                        url.set("https://github.com/theshadowco")
+                        organization.set("1c-syntax")
+                        organizationUrl.set("https://github.com/1c-syntax")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/1c-syntax/supportconf.git")
+                    developerConnection.set("scm:git:git@github.com:1c-syntax/supportconf.git")
+                    url.set("https://github.com/1c-syntax/supportconf")
+                }
+            }
+        }
+    }
+}
+
+tasks.register("precommit") {
+    description = "Run all precommit tasks"
+    group = "Developer tools"
+    dependsOn(":test")
+    dependsOn(":licenseFormat")
+}
+
+tasks.withType<Javadoc> {
+    (options as StandardJavadocDocletOptions)
+        .addStringOption("Xdoclint:none", "-quiet")
+}
---
+++ src/main/java/com/github/_1c_syntax/bsl/supconf/GeneralSupportVariant.java
@@ -0,0 +1,39 @@
+/*
+ * This file is a part of Support Configuration.
+ *
+ * Copyright © 2019 - 2022
+ * Tymko Oleg <olegtymko@yandex.ru>, Maximov Valery <maximovvalery@gmail.com> and contributors
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * Support Configuration is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * Support Configuration is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Support Configuration.
+ */
+package com.github._1c_syntax.bsl.supconf;
+
+public enum GeneralSupportVariant {
+  LOCKED, UNLOCKED;
+
+  /**
+   * Находит элемент по приоритету
+   *
+   * @param priority номер приоритета
+   * @return Найденное значение
+   */
+  public static GeneralSupportVariant valueOf(int priority) {
+    if (priority == 0) {
+      return UNLOCKED;
+    }
+    return LOCKED;
+  }
+}
