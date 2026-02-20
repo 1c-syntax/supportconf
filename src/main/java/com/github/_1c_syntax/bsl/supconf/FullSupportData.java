03044710f5003be67e5ee64a04dc61e21beff830
Maximov Valery <maximovvalery@gmail.com>
2026-02-20 18:01:25 +0300

    Обновления (#123)
    * лицензии и обвноления зависимостей
    
    * null check jspecify

--- src/main/java/com/github/_1c_syntax/bsl/supconf/FullSupportData.java
+++ src/main/java/com/github/_1c_syntax/bsl/supconf/FullSupportData.java
@@ -1,7 +1,7 @@
 /*
  * This file is a part of Support Configuration.
  *
- * Copyright (c) 2019 - 2025
+ * Copyright (c) 2019 - 2026
  * Tymko Oleg <olegtymko@yandex.ru>, Maximov Valery <maximovvalery@gmail.com> and contributors
  *
  * SPDX-License-Identifier: LGPL-3.0-or-later
@@ -52,7 +52,8 @@ public final class FullSupportData {
     this(pathParentConfigurationBin, Collections.emptyMap());
   }
 
-  private FullSupportData(Path pathParentConfigurationBin, Map<String, Map<SupportConfiguration, SupportVariant>> supportVariants) {
+  private FullSupportData(Path pathParentConfigurationBin,
+                          Map<String, Map<SupportConfiguration, SupportVariant>> supportVariants) {
     this.supportVariants = supportVariants;
     this.pathParentConfigurationBin = pathParentConfigurationBin;
   }
